9d854627eb14fb42ddc705747a5c56f0246d390b
starfighter-ix <ec14370@qmul.ac.uk>
2018-03-11 17:02:22 +0000

    Adding table to template showing guest list.

--- qmsalsa_drapers_registration/qmsalsa_drapers_registration/urls.py
+++ qmsalsa_drapers_registration/qmsalsa_drapers_registration/urls.py
@@ -17,6 +17,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('qmsalsa/', include('reg_guests.urls')),
+    path('', include('reg_guests.urls')),
     path('admin/', admin.site.urls),
 ]
