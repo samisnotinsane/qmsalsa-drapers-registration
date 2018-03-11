9502e41d5d8a4a4ea0b9abbbd1f4ef0defe92ce9
starfighter-ix <ec14370@qmul.ac.uk>
2018-03-11 15:03:04 +0000

    Adding model for guests.

--- qmsalsa_drapers_registration/qmsalsa_drapers_registration/settings.py
+++ qmsalsa_drapers_registration/qmsalsa_drapers_registration/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reg_guests',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- qmsalsa_drapers_registration/reg_guests/models.py
+++ qmsalsa_drapers_registration/reg_guests/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Ask for the following fields from guests:
+# First name
+# Surname
+# Email
+# Instagram
+# Have you done any salsa before?
+# Amount paid
+
+class Guest():
+    first_name = models.CharField(null=False, max_length=50)
+    last_name = models.CharField(null=False, max_length=50)
+    email = models.CharField(max_length=150)
+    instagram = models.CharField(max_length=150)
+    is_new_to_salsa = models.BooleanField(default=True)
+    amount_paid = models.DecimalField(null=False, max_digits=3, decimal_places=2)
+
