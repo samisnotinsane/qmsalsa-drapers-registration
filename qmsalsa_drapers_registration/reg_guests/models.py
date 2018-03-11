564c4a276f8522b3f71c854183996641a753c935
starfighter-ix <ec14370@qmul.ac.uk>
2018-03-11 15:14:29 +0000

    Registering model to admin.

--- qmsalsa_drapers_registration/reg_guests/models.py
+++ qmsalsa_drapers_registration/reg_guests/models.py
@@ -8,7 +8,7 @@ from django.db import models
 # Have you done any salsa before?
 # Amount paid
 
-class Guest():
+class Guest(models.Model):
     first_name = models.CharField(null=False, max_length=50)
     last_name = models.CharField(null=False, max_length=50)
     email = models.CharField(max_length=150)
@@ -16,3 +16,5 @@ class Guest():
     is_new_to_salsa = models.BooleanField(default=True)
     amount_paid = models.DecimalField(null=False, max_digits=3, decimal_places=2)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name + ", " + self.email + ", " + self.amount_paid
